196d0405298b2811362f0a1e1c00dfc2614d70e9
Hamza Sadiq <hamzasadiq6789@gmail.com>
2024-06-12 04:52:57 +0500

    2nd commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Word VS Keyword 
+// var let const 
+// hoisting
+// Types in js
+// Primitives and Refernce
+// Conditional - if else else-if
+// Loops  -For While
+// Functions  
+             //Arguments= real value jo ham dety hai functioin chalaty wqt 
+             //Parameter= Variables jin mn value store hoti hai arguments wali 
+// Arrays = group of values
+                           // Push,Pop,Shift,Unshift,splice
+// objects          
+          // property vs Method
+          //updating object property
+
